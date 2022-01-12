b845e1b2e8d3a67d9f3b713224268a78baf6538d
Gianna Smith <gnsmith127@gmail.com>
2022-01-12 15:37:30 -0500

    finished writing method and test

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,21 @@
 using System;
+using System.Linq;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)] 
+        [InlineData ("hello", false)]
+        [InlineData ("tenet", true)]
+        public void IsAPalindrome(string word, bool expected)
         {
-
+            var test = new WordSmith();
+            bool actual = test.IsAPalindrome(word);
+            Assert.Equal(actual, expected);
         }
     }
 }
